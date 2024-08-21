d73aac9fe46ae084b727360b4e7b51d727189341
MuhammadTabish <muhammad.tabish@paysyslabs.com>
2024-08-21 17:14:28 +0500

    BaseSecurity Dependency Project added

---
+++ OtherServices/CommonModels/src/main/java/com/healthconnect/commonmodels/model/Permission.java
@@ -0,0 +1,25 @@
+package com.healthconnect.commonmodels.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "permissions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+}
